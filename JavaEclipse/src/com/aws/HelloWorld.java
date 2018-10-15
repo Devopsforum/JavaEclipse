03de43c4fdd57fe2f9a9c42637bfb4629d64cde4
Devopsforum <devopsforum@gmail.com>
2018-10-15 19:43:56 -0400

    Hello world

---
+++ JavaEclipse/src/com/aws/HelloWorld.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package com.aws;
+
+/**
+ * @author Yvs Venkat
+ *
+ */
+public class HelloWorld {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Hello DevOps Forum");
+
+	}
+
+}
